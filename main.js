9fea8cc2fc5586c9bbfcfc830b25e913d492b962
Rohan Pujari <rohanpujaris@gmail.com>
2015-02-17 23:57:25 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var app = angular.module('myApp', [])
+app.controller('personController', function($scope){
+  // $scope.name= 'dada'
+})
+app.controller('personController1', function($scope){
+  // $scope.name= 'dada222121'
+})
